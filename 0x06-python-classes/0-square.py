aafaf1f7a0f60acf2270421aa8948a16fc0b5ded
EmmaTBxx <maneyras@gmail.com>
2023-10-25 16:11:54 -0700

    k

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 
 class Square:
-    """A class that defines a square."""
+    """A class representing a square.
+
+    class defines a square and provides methods to calc its area and perimeter.
+
+    Attributes:
+        side_length (int): The length of each side of the square.
+    """
 
     def __init__(self, side_length):
-        """
-        Initializes a square with a given side length.
+        """Initializes a Square instance.
 
         Args:
             side_length (int): The length of each side of the square.
@@ -13,8 +18,7 @@ class Square:
         self.side_length = side_length
 
     def area(self):
-        """
-        Calculates and returns the area of the square.
+        """Calculates and returns the area of the square.
 
         Returns:
             int: The area of the square.
@@ -22,8 +26,7 @@ class Square:
         return self.side_length ** 2
 
     def perimeter(self):
-        """
-        Calculates and returns the perimeter of the square.
+        """Calculates and returns the perimeter of the square.
 
         Returns:
             int: The perimeter of the square.
