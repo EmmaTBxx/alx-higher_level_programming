2ed792bb0cfdedc1ddbd6be271dca6f57d363104
EmmaTBxx <maneyras@gmail.com>
2023-10-25 09:56:08 -0700

    kk

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 
-def safe_print_division(a, b):
-    try:
-        div = a / b
-    except (ZeroDivisionError, FloatingPointError):
-        div = None
-    finally:
-        print("Result of division: {}".format(div))
-    return div
+def safe_print_list_integers(my_list=[], x=0):
+    """
+    Prints the first x elements of a list as integers.
+
+    Args:
+        my_list: The list to print elements from.
+        x: Number of elements to access in my_list.
+
+    Returns: The real number of integers printed.
+    """
+    count = 0
+    for item in range(x):
+        if isinstance(my_list[item], int):
+            try:
+                print("{:d}".format(my_list[item]), end=' ')
+                count += 1
+            except (IndexError, ValueError):
+                break
+    print()
+    return count
